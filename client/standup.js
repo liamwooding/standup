68f69d5b1ab182343de388f66a67903ca44c394e
liamwooding <liam@ptasocial.com>
2013-11-07 17:26:29 +0000

    Got the standup app working

--- client/standup.js
+++ client/standup.js
@@ -1,13 +1,25 @@
-if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to standup.";
-  };
+Template.todayText.events({
+  'keyup #today' : function (event) {
+    var todayText = ($('#today textarea').val());
+    Meteor.users.update(Meteor.user()._id, {$set: {'profile.data.today': todayText}});
+  }    
+});
 
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
-}
+Template.yesterdayText.events({
+  'keyup #yesterday' : function (event) {
+    var yesterdayText = ($('#yesterday textarea').val());
+    Meteor.users.update(Meteor.user()._id, {$set: {'profile.data.yesterday': yesterdayText}});
+  }    
+});
+
+Handlebars.registerHelper('user', function() {
+  return Meteor.user();
+})
+
+Handlebars.registerHelper('theDate', function() {
+  return moment().format('MMMM Do YYYY');
+})
+
+Template.team.users = function () {
+  return Meteor.users.find();
+};
--- server/server.js
+++ server/server.js
@@ -1,5 +1,12 @@
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
+  	Meteor.setInterval(function() {
+  		if (moment().startOf('day').diff(moment()) > -3000) {
+  			Meteor.users.find().forEach(function(user) {
+  				Meteor.users.update(user._id, {$set: {'profile.data.yesterday': user.profile.data.today}});
+  				Meteor.users.update(user._id, {$set: {'profile.data.today': ''}});
+  			});
+  		}
+  	}, 3000);
   });
 }
